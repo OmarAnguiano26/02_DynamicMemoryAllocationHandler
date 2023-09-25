b5e4ad8385924ad339f8fb1eacb3fe9fee06adf7
OmarAnguiano26 <ie702883@iteso.mx>
2023-09-25 01:07:09 -0600

    Base code with files

---
+++ app/02_MemoryAllocation/src/Mem_Alloc.c
@@ -0,0 +1,12 @@
+/*******************************************************************************/
+/**
+\file       Mem_Malloc.c
+\brief      Dynamic Memory Allocation.
+\author     Omar Anguiano / Diego Reyna
+\version    0.1
+\date       09/25/2023
+*/
+
+/** Variable types and common definitions */
+#include "system_samv71.h"
+#include "Mem_Alloc.h"
